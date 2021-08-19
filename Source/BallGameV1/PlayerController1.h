b929de5741f47601e316c44e5cc7011c7c1fb513
Tom Lightfoot <tomlightfootr@gmail.com>
2021-08-19 12:25:45 +1200

    commit of source files

---
+++ Source/BallGameV1/PlayerController1.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PlayerController1.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BALLGAMEV1_API APlayerController1 : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
